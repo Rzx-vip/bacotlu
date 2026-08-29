98084052026051c8e0e8bcce59d9958d23d6b188
RZX - REZZX NO SEPUH <yatanti96@gmail.com>
2026-08-29 22:12:48 +0700

    Add files via upload

---
+++ YANZ MD VERSION TELEGRAM 1.2/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // ─── 🔐 DATA UTAMA ───
+  BOT_TOKEN: "8983060390:AAFXPVv9dL92LSVLnx4-gay_JrMt4N4vyp0",
+  OWNER_ID: 5969597835,
+  BOT_IMAGE: 'https://files.catbox.moe/6ogiaj.jpg',
+
+  // ─── 🤖 AI & MAKER CONFIGURATION ───
+  AI_API_URL: 'https://api.siputzx.my.id/api/ai/chatgpt',
+  BRAT_API_URL: 'https://api.siputzx.my.id/api/maker/brat?text=',
+
+  // ─── 🛡️ FITUR PROTEKSI GRUP ───
+  ANTILINK: true,       // true = aktif, false = nonaktif
+  ANTITOXIC: true,      // true = aktif, false = nonaktif
+  ANTIPROMOSI: true,    // true = aktif, false = nonaktif
+
+  // ─── 🎨 SETTING TOMBOL ───
+  DEFAULT_BUTTON_STYLE: 'primary',
+  EMOJI_MAP: {},
+
+  // ─── ℹ️ INFO SCRIPT ───
+  NAMA_SCRIPT: 'YANZ MD TELEGRAM',
+  VERSI: '1.2'
+};
